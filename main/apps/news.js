997ec692ea295a2302be64981b0e0081af0eb00c
frcastillo <franklin.r.castillo.3@gmail.com>
2020-06-25 12:38:19 -0400

    updated news.js

--- main/apps/news.js
+++ main/apps/news.js
@@ -4,7 +4,7 @@ export async function news() {
     var arr = [];
 
     for (var i = 0; i < url.length; i++) {
-        var xml = await ReadFile(arr[i]);
+        var xml = await ReadFile(url[i]);
         var tmp = RSSParser(xml);
 
         for (var j = 0; j < tmp.length; j ++) {
