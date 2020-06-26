70f2959b2418790e4d952c8566817817a8aad5f5
frcastillo <franklin.r.castillo.3@gmail.com>
2020-06-26 15:28:31 -0400

    update apps.js

--- main/apps/apps.js
+++ main/apps/apps.js
@@ -2,8 +2,9 @@ export async function apps() {
     var gapi = 'https://api.github.com/repos/FrankRCastillo/frankrcastillo.github.io/git/trees/master?recursive=1';
     var text = await ReadFile(gapi);
     var json = JSON.parse(text);
+    var tree = json.tree;
 
-    for (var elem in json) {
+    for (var elem in tree) {
         print(elem.path);
     }
 
