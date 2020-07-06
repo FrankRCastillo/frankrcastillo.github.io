3a6ed089e04e51f9ad10f6789e92f867963a3dab
frcastillo <franklin.r.castillo.3@gmail.com>
2020-07-06 11:32:06 -0400

    update

--- main/hist.js
+++ main/hist.js
@@ -4,7 +4,7 @@ export async function hist() {
     var url = 'https://www.cia.gov/library/publications/resources/the-world-factbook/fields/325.html'
     var txt = await ReadFile(url);
     var doc = document.implementation.createHTMLDocument('New Document');
-    p.outerHTML = txt;
+    doc.outerHTML = txt;
 
         
 }
