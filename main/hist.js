fc3ca3784d89d277413d3d1b1d6fa59d555f058f
frcastillo <franklin.r.castillo.3@gmail.com>
2020-07-06 11:50:14 -0400

    update

--- main/hist.js
+++ main/hist.js
@@ -4,7 +4,17 @@ export async function hist() {
     var url = 'https://www.cia.gov/library/publications/resources/the-world-factbook/fields/325.html'
     var txt = await ReadFile(url);
     var doc = document.implementation.createHTMLDocument('New Document');
+
     doc.outerHTML = txt;
 
-        
+    var lst = doc.querySelector('#fieldListing');
+    var tag = lst.getElementsByTagName('tr');
+    var arr = [];
+
+    for (var cty in tag) {
+        var name = cty.querySelector('.country').innerText;
+        var hist = cty.querySelector('#field-background').innerText;
+
+        arr.push([cty.id, name, hist]);
+    }
 }
