2d408cbe52bc2e18376be6ccef21ee1544bae4c1
frcastillo <franklin.r.castillo.3@gmail.com>
2020-07-06 02:58:41 -0400

    update

--- js/page.js
+++ js/page.js
@@ -2,19 +2,22 @@ function NewObject(prnt, attr) {
     for (var i = 0; i < attr.length; i++) {
         var child = document.createElement(attr[i][0]);
 
-        for (var j = 0; j < attr[i][2]; i++) {
-            switch (attr[i][2][0]) {
+        for (var j = 0; j < attr[i][2]; j++) {
+            var atttype = attr[i][2][j][0];
+            var attrval = attr[i][2][j][1];
+
+            switch (attrval) {
                 case 'text':
-                    var text = document.createTextNode(attr[i][2][1]);
+                    var text = document.createTextNode(attrval);
                     child.appendChild(text);
                     break;
 
                 case 'object':
-                    child.appendChild(attr[i][2][1]);
+                    child.appendChild(attrval);
                     break;
 
                 default:
-                    child.setAttribute(attr[i][2][0], attr[i][2][1]);
+                    child.setAttribute(atttype, attrval);
                     break;
             }
         }
