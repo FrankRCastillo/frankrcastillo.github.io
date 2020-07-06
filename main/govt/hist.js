17ee879982365f1cd7bc66a2bf9dc91778f1d8c3
frcastillo <franklin.r.castillo.3@gmail.com>
2020-07-06 04:04:20 -0400

    update

--- main/govt/hist.js
+++ main/govt/hist.js
@@ -1,3 +1,10 @@
 // |hist|World history infographic (powered by CIA World Factbook)
 
+export async hist() {
+    var url = 'https://www.cia.gov/library/publications/resources/the-world-factbook/fields/325.html'
+    var txt = await ReadFile(url);
+    var doc = document.implementation.createHTMLDocument('New Document');
+    p.outerHTML = txt;
 
+        
+}
