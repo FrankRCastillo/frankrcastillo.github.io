aeebd048fefcb69321101861c7fc92bf4485a5f6
frcastillo <franklin.r.castillo.3@gmail.com>
2020-07-08 19:39:26 -0400

    update

--- main/hist.js
+++ main/hist.js
@@ -26,13 +26,13 @@ function parseHistory(str) {
                  , "[A-Z]{1}[a-z]{1,3}(\\.) (?!King)([A-Z]{1}[a-z]{1,} ){0,}[A-Z]{2,}" // ranks and titles; excludes the title of King
                  ]
     
-    var tmp = rgxexp.map(function(exp, i) {
+    var tmp = rgxexp.map(function(exp) {
         var rgxdom = new RegExp(exp);
         var rgxget = str.match(rgxdom);
 
         try {
-            var rgxmod = rgxget.map(function(rgxitm, j)) {
-                if (j = 0) {
+            var rgxmod = rgxget.map(function(rgxitm, i)) {
+                if (i == 0) {
                     var rgxarr = rgxitm.split('.');
                     var penult = rgxarr.slice(0, rgxarr.length - 2).join('');
                     var ultima = rgxarr[rgxarr.length - 1];
