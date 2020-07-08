f0f502a4174f68616c05bdfe00cafc27c9098e11
frcastillo <franklin.r.castillo.3@gmail.com>
2020-07-07 23:51:32 -0400

    update

--- main/hist.js
+++ main/hist.js
@@ -19,3 +19,32 @@ export async function hist() {
 
     console.log('pause');
 }
+
+function parseHistory(str) {
+    var rgxexp = [ "([A-Za-z](\.)){2,} [A-Z]"	                    			        // acronyms at the end of a sentence; delete all but last period
+                 , "([A-Za-z](\.)){2,} [^A-Z]"	                        				// acronyms within a sentece, but not the end; delete all periods
+                 , "[A-Z]{1}[a-z]{1,3}(\.) (?!King)([A-Z]{1}[a-z]{1,} ){0,}[A-Z]{2,}"	// ranks and titles; excludes the title of King
+                 ]
+    
+    var tmp = str;
+
+    for (var i = 0; i < rgxexp.length; i++) {
+        var rgxdom = new RegExp(rgxexp[i]);
+        var rgxget = tmp.match(rgxdom);
+        
+        for (var j = 0; j < rgxget.length; j++) {
+            var rgxmod = '';
+
+            if (i == 0) {
+                rgxmod = rgxget[i].replace(/[.](?=.*[.])/g, '');
+
+            } else {
+                rgxmod = rgxget[i].replace('.', '');
+            }
+
+            tmp = tmp.replace(rgxget[j], rgxmod);
+        }
+    }
+
+    return tmp.split('. ');
+}
