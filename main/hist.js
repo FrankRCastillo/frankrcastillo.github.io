19703d420d696230b756cd7a396e6b051baa520e
frcastillo <franklin.r.castillo.3@gmail.com>
2020-07-09 22:09:49 -0400

    update

--- main/hist.js
+++ main/hist.js
@@ -17,13 +17,14 @@ export async function hist() {
         arr.push([id, name, harr]);
     }
 
-    console.log('pause');
+    historychart(arr);
+    cmdReady();
 }
 
 function parseHistory(str) {
-    var rgxexp = [ "([A-Za-z](\.)){2,} [A-Z]"                                         // acronyms at the end of a sentence; delete all but last period
-                 , "([A-Za-z](\.)){2,} [^A-Z]"                                        // acronyms within a sentece, but not the end; delete all periods
-                 , "[A-Z]{1}[a-z]{1,3}(\.) (?!King)([A-Z]{1}[a-z]{1,} ){0,}[A-Z]{2,}" // ranks and titles; excludes the title of King
+    var rgxexp = [ "([A-Za-z](\\.)){2,} [A-Z]"                                         // acronyms at the end of a sentence; delete all but last period
+                 , "([A-Za-z](\\.)){2,} [^A-Z]"                                        // acronyms within a sentece, but not the end; delete all periods
+                 , "[A-Z]{1}[a-z]{1,3}(\\.) (?!King)([A-Z]{1}[a-z]{1,} ){0,}[A-Z]{2,}" // ranks and titles; excludes the title of King
                  ]
     
     var tmp = str;
@@ -36,8 +37,12 @@ function parseHistory(str) {
             for (var j = 0; j < rgxget.length; j++) {
                 var rgxmod = '';
 
+                // first regex requires that all but last periods be removed
                 if (i == 0) {
-                    rgxmod = rgxget[j].replace(/[.](?=.*[.])/g, '');
+                    var rgxarr = rgxget[j].split('.');
+                    var penult = rgxarr.slice(0, rgxarr.length - 2).join('');
+                    var ultima = rgxarr[rgxarr.length - 1];
+                    rgxmod = penult + '.' + ultima;
 
                 } else {
                     rgxmod = rgxget[j].replace('.', '');
@@ -45,16 +50,55 @@ function parseHistory(str) {
 
                 tmp = tmp.replace(rgxget[j], rgxmod);
             }
-        } get(err) {
+        } catch(err) {
             console.log(err.message);
         }
     }
 
     var arr = tmp.split('. ');
-    var rtn = arr.map(function(elem, i) {
-        var info = 'info';
-        return [info, elem];
+
+    var rtn = arr.map(function(elem, i, orig) {
+        var yrgx = elem.match(/([1](?<=1)[0-9]|20)[0-9]{2}/)
+        var info = Math.max.apply(null, yrgx);
+
+        if (Number.isFinite(info) == false && i > 0) {
+            yrgx = orig[i - 1].match(/([1](?<=1)[0-9]|20)[0-9]{2}/)
+            info = Math.max.apply(null, yrgx);
+        }
+        
+        return [info, elem + '.'];
     });
 
     return rtn;
+}
+
+function historychart(arr) {
+    var out = document.getElementById('outtext');
+    var frm = document.createElement('div');
+    var lst = document.createElement('select');
+
+    frm.setAttribute('id'      , 'mapframe');
+    lst.setAttribute('id'      , 'maplist' );
+    lst.setAttribute('name'    , 'maplist' );
+    lst.setAttribute('multiple', true      );
+
+    for (var i = 0; i < arr.length; i++) {
+        var opt = document.createElement('option');
+        opt.setAttribute('value', arr[i][0]);
+        opt.textContent = arr[i][1];
+        lst.appendChild(opt);
+    }
+    
+    out.appendChild(lst);
+    out.appendChild(frm);
+
+    var map = new Datamap({ element         : document.getElementById('mapframe')
+                          , scope           : 'world'
+                          , fills           : { defaultFill      : '#ffffff' }
+                          , geographyConfig : { highlightOnHover : false
+                                              , popupOnHover     : false
+                                              , borderColor      : '#000000'
+                                              , borderWidth      : 1
+                                              }
+    });
 }
