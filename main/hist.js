98dddadb0c8917525e78cdb83b6c5bad41991029
frcastillo <franklin.r.castillo.3@gmail.com>
2020-07-12 19:49:48 -0400

    update

--- main/hist.js
+++ main/hist.js
@@ -97,7 +97,7 @@ function historychart(arr) {
         var ico = document.createElement('button');
         ico.textContent = zmCmd[i];
         ico.setAttribute('class', 'zoombtn');
-        ico.setAttribute('click', 'javascript:mapzoom("' + zmCmd[i] + '")');
+        ico.setAttribute('onclick', 'javascript:mapzoom("' + zmCmd[i] + '")');
         zbt.appendChild(ico);
     }
 
