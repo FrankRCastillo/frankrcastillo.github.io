64842ab79ea35cab87d794da58fe2f4d6058087f
frcastillo <franklin.r.castillo.3@gmail.com>
2020-07-14 20:00:30 -0400

    update

--- js/datamaps.world.hires.js
+++ js/datamaps.world.hires.js
@@ -1134,8 +1134,8 @@
     element.on('mousemove', function() {
       var position = d3.mouse(self.options.element);
       d3.select(self.svg[0][0].parentNode).select('.datamaps-hoverover')
-        .style('top'  , ((position[1] - 30)) + "px")
-        .style('left' , ((position[1] - 30)) + "px")
+        .style('left' , ((position[0] + 30)) + "px")
+        .style('top'  , ((position[1] + 30)) + "px")
         .html(function() {
           var data = JSON.parse(element.attr('data-info'));
           try {
