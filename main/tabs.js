4552731222760a3ff551053c6bb1b8f21fe4ed7b
frcastillo <franklin.r.castillo.3@gmail.com>
2020-07-17 23:37:30 -0400

    update

--- main/tabs.js
+++ main/tabs.js
@@ -3,11 +3,17 @@
 export async function tabs() {
     var toprow = [ 'Map', 'Gantt' ];
     var maprow = [ 'M', 'A', 'P' ];
-    var gntrow = [ 'G', 'A', 'N', 'T', 'T' ];
+    var gntrow = [ 'G', 'N', 'T' ];
 
     document.getElementById('outtext').appendChild(NewTabLayout(toprow));
     document.getElementById(    'Map').appendChild(NewTabLayout(maprow));
     document.getElementById(  'Gantt').appendChild(NewTabLayout(gntrow));
+    document.getElementById('M').textContent = 'M';
+    document.getElementById('A').textContent = 'A';
+    document.getElementById('P').textContent = 'P';
+    document.getElementById('G').textContent = 'G';
+    document.getElementById('N').textContent = 'N';
+    document.getElementById('T').textContent = 'T';
 }
 
 
