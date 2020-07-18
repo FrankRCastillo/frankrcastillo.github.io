8e824e564da846014c82fa2d4cdbbc5907846641
frcastillo <franklin.r.castillo.3@gmail.com>
2020-07-17 22:02:26 -0400

    update

--- main/tabs.js
+++ main/tabs.js
@@ -2,10 +2,12 @@
 
 export async function tabs() {
     var toprow = [ 'Map', 'Gantt' ];
-    var midrow = [ 'G', 'A', 'N', 'T', 'T' ]
+    var maprow = [ 'M', 'A', 'P' ];
+    var gntrow = [ 'G', 'A', 'N', 'T', 'T' ];
 
     document.getElementById('outtext').appendChild(NewTabLayout(toprow));
-    document.getElementById(    'Map').appendChild(NewTabLayout(midrow));
+    document.getElementById(    'Map').appendChild(NewTabLayout(maprow));
+    document.getElementById(  'Gantt').appendChild(NewTabLayout(gntrow));
 }
 
 
