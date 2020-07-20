e334cd8d4ec927e8af202022ae29861bc1b9211e
frcastillo <franklin.r.castillo.3@gmail.com>
2020-07-20 18:03:59 -0400

    update

--- main/ctry.js
+++ main/ctry.js
@@ -26,7 +26,7 @@ function CreateMap() {
                , { attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>contributors'
 	}).addTo(map);
 
-    map.setView([0, 50], 2);
+    map.setView([50, 0], 2);
 }
 
 async function GenerateData() {
