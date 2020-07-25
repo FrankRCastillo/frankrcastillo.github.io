c96d2ef2ba4ef9bbde78ee5fcdcc44f16d2dbb80
frcastillo <franklin.r.castillo.3@gmail.com>
2020-07-25 16:09:30 -0400

    update

--- main/ctry.js
+++ main/ctry.js
@@ -316,12 +316,12 @@ function NewGanttPage(year, scale) {
                     ico.setAttribute(     'id', 'ctryEvent');
                     ico.setAttribute(  'class',   'tooltip');
                     ico.setAttribute('ctryidx',           i);
-                    ico.setAttribute('yearidx',     yearRng);
+                    ico.setAttribute('yearidx',           j);
                     ico.addEventListener('mouseover', function() {
                         var endYear = document.getElementById('GanttEndYear');
                         var intYear = document.getElementById('GanttInterval');
                         var ctryidx = parseInt(this.getAttribute('ctryidx'));
-                        var yearidx = this.getAttribute('yearidx').split(',')[0];
+                        var yearidx = parseInt(this.getAttribute('yearidx'));
                         var hist    = document.createElement('span');
                         
                         hist.innerHTML = window.ctryData[ctryidx][5];
@@ -335,7 +335,7 @@ function NewGanttPage(year, scale) {
                             yearArr.sort((a,b) => b - a);
 
                             for (var k = 0; k < yearArr.length; k++) {
-                                if (yearidx.includes(yearArr[k])) {
+                                if (yearArr[k] == yearidx) {
                                     return x.innerHTML;
                                 }
                             }
