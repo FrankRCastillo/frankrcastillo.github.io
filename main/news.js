50d3924d8a21e106b3440f6cc271c8823bb13cbd
frcastillo <franklin.r.castillo.3@gmail.com>
2020-07-27 00:12:22 -0400

    update

--- main/news.js
+++ main/news.js
@@ -1,40 +1,37 @@
 // |apps|news|RSS feed from various news sources
 
 export async function news() {
-    var src = await ReadFile('/main/news/news.txt');
-    var url = src.split('\n');
-    var arr = [];
+    var out = document.getElementById('outtext');
 
-    for (var i = 0; i < url.length; i++) {
-        try {
-            var xml = await ReadFile(url[i]);
-            var tmp = RSSParser(xml);
+    window.interval = setInterval(function() {
+        var arr = GetNewsFeed();
+        var tbl = ArrayToTable(arr, false, true);
+        tbl.setAttribute('id', 'NewsTable');
+        out.appendChild(tbl);
+    }, 60000);
+}
 
-            for (var j = 0; j < tmp.length; j ++) {
-                arr.push(tmp[j]);
-            }
-        } catch {
-            console.log(url[i] + ' not available');
-        }
-    }
+async function GetNewsFeed() {
+    var src  = await ReadFile('/main/news/news.txt');
+    var urls = src.split('\n');
+    var arr  = urls.map(function (url) {
+        var xml = await ReadFile(url);
+        return xml.map(data => RSSParser(data));
+    });
 
-    arr.sort((a, b) => Date.parse(b[2]) - Date.parse(a[2]));
-    
-    var tmp = arr.map(x => [ x[0]
-                           , x[1]
-                           , new Intl.DateTimeFormat( 'en-US'
-                                                    , { month  : '2-digit'
-                                                      , day    : '2-digit'
-                                                      , hour   : '2-digit'
-                                                      , minute : '2-digit'
-                                                      , hour12 : false
-                                                      }).format(new Date(x[2]))
-                                                        .replace(',', '')
-                           , x[3]
-                           ]);
+    arr.sort((a,b,) => Date.parse(b[2]) - Date.parse(a[2]));
 
-    var tbl = ArrayToTable(tmp, false, true);
-    tbl.setAttribute('id', 'NewsTable');
-    clear();
-    document.getElementById('outtext').appendChild(tbl);
+    return arr.map(x => [ x[0]
+                        , x[1]
+                        , new Intl.DateTimeFormat(
+                            'en-US'
+                             , { month  : '2-digit'
+                               , day    : '2-digit'
+                               , hour   : '2-digit'
+                               , minute : '2-digit'
+                               , hour12 : false
+                               }).format(new Date(x[2]))
+                                 .replace(',', '')
+                        , x[3]
+                        ]);
 }
