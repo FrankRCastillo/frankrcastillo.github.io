27010d84bea6dbb7ff629562fe3297a1b6be6020
frcastillo <franklin.r.castillo.3@gmail.com>
2020-07-28 14:28:15 -0400

    update

--- apps/lead.js
+++ apps/lead.js
@@ -3,8 +3,11 @@
 export async function lead() {
     var url = "https://www.cia.gov/library/publications/world-leaders-1/";
     var txt = await ReadFile(url);
-    var doc = new DOMParser().parseFromString(txt, 'text/html');
-    var lnk = doc.getElementsByTagName('a');
+    var dom = new DOMParser().parseFromString(txt, 'text/html');
+    var bse = dom.createElement('base');
+    bse.setAttribute('href', url);
+    dom.head.append(bse);
+    var lnk = dom.getElementsByTagName('a');
     var pdf = Array.from(lnk).map(x => x.href);
 
     clear();
