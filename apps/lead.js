878bd4b092bb16ee6bd6becb9a51af1af74da251
frcastillo <franklin.r.castillo.3@gmail.com>
2020-07-28 16:09:51 -0400

    update

--- apps/lead.js
+++ apps/lead.js
@@ -1,6 +1,20 @@
 // |wrld|lead|Listing of government leaders (national, provincial, local; source: CIA WFB)
 
 export async function lead() {
+   var mth = [ 'January'
+             , 'February'
+             , 'March'
+             , 'April'
+             , 'May'
+             , 'June'
+             , 'July'
+             , 'August'
+             , 'September'
+             , 'October'
+             , 'November'
+             , 'December'
+             ];
+    
     var url = "https://www.cia.gov/library/publications/world-leaders-1/";
     var txt = await ReadFile(url);
     var dom = new DOMParser().parseFromString(txt, 'text/html');
@@ -8,8 +22,15 @@ export async function lead() {
     bse.setAttribute('href', url);
     dom.head.append(bse);
     var lnk = dom.getElementsByTagName('a');
-    var pdf = Array.from(lnk).map(x => x.href).filter(x => x.endsWith('.pdf'));
+    var tmp = Array.from(lnk)
+                   .map(x => x.href)
+                   .filter(x => x.match('.*ChiefsDirectory.pdf'));
+    var pdf = tmp.map(function(x){
+        var s = x.split('/');
+        return [ s[7], s[8].split(s[7])[0], x]
+    });
 
     clear();
     print(pdf);
 }
+
