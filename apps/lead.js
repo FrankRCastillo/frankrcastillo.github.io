0acac241ce2ac4654efa3e7e2479bf9af040be0a
frcastillo <franklin.r.castillo.3@gmail.com>
2020-07-29 15:35:11 -0400

    update

--- apps/lead.js
+++ apps/lead.js
@@ -20,11 +20,11 @@ async function readPdf(url) {
     var wht = { normalizeWhitespace : true };
     var doc = await pdfjsLib.getDocument(bin).promise;
     var txt = Array.from({length : doc.numPages}, async (x, i) => {
-        return (await (await doc.getPage(i + 1)
-                                .getTextContent()
+        return (await (await doc.getPage(i + 1))
+                                .getTextContent())
                                 .items
                                 .map(token =>  token.str)
-                                .join('')));
+                                .join('');
     });
 
     return (await Promise.all(txt)).join('');
