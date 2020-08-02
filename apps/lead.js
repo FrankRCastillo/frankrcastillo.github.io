0705ae27b6042d125b8629a1367f100d5a388c2c
frcastillo <franklin.r.castillo.3@gmail.com>
2020-08-01 22:50:30 -0400

    update

--- apps/lead.js
+++ apps/lead.js
@@ -3,11 +3,12 @@
 export async function lead() {
     var iso = TableToArray(await ReadFile('/js/iso.tsv'), '\t');                            // read csv file with iso2 to iso3 table and convert to array
     var arr = await FileList(/apps\/lead\/2015\.05\.pdf/);
-    var out = arr.map(async function(x) {
+    var out = arr.map(async function(x){
         var txt = await readPdf(x);
-        return parsePages(txt, iso);
+        var prs = parsePages(txt, iso)
+        return prs;
     });
-    
+
     print("Under construction");
 }
 
