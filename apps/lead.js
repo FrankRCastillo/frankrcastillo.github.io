a000ce315d7550d725adc146e857ed544ee4d244
frcastillo <franklin.r.castillo.3@gmail.com>
2020-08-05 02:22:04 -0400

    update

--- apps/lead.js
+++ apps/lead.js
@@ -22,7 +22,7 @@ export async function lead() {
                                                          ));
     });
     
-    console.log("pause");
+    print(prs);
 }
 
 function pageToArray(arr){
