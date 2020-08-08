0c64833d6e3dacb99f82c7eac1d0af5873dbc36e
frcastillo <franklin.r.castillo.3@gmail.com>
2020-08-07 23:54:02 -0400

    update

--- apps/lead.js
+++ apps/lead.js
@@ -4,8 +4,7 @@ export async function lead() {
     var url = 'http://rulers.org/index.html';
     var get = readFile(url);
     var dom = new DOMParser().parseFromString(get, 'text/html')
-                             .getElementsByTagName('a');
- 
+                             .getElementsByTagName('a'); 
     var arr = Array.from(dom)
                    .map(x => x.href)
                    .filter(x => x.match('rul.*\.html'));
