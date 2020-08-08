923e52959cd8c1af0e587be746b6ef50d199c8a2
frcastillo <franklin.r.castillo.3@gmail.com>
2020-08-08 00:09:23 -0400

    update

--- apps/lead.js
+++ apps/lead.js
@@ -1,12 +1,12 @@
 // |wrld|lead|National leaders and tenures (source: rulers.org)
 
 export async function lead() {
-    var url = 'http://rulers.org/index.html';
+    var url = 'http://rulers.org/';
     var get = await readFile(url);
     var dom = new DOMParser().parseFromString(get, 'text/html')
                              .getElementsByTagName('a'); 
     var arr = Array.from(dom)
-                   .map(x => dom.baseURI + x.href)
+                   .map(x => x.href.replace(x.baseURI, url))
                    .filter(x => x.match('rul.*\.html'));
 
     console.log('pause');
