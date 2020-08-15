fc269153d28e2219e10c5de4a87f07600f72f72a
Frank Castillo <franklin.r.castillo.3@gmail.com>
2020-08-15 12:43:32 -0400

    update

--- apps/news.js
+++ apps/news.js
@@ -27,8 +27,7 @@ export function news() {
 export async function getNewsFeed() {
     var src = await readFile('/apps/news/news.txt');
     var url = src.split('\n').filter(x => x != '');
-    var xml = url.map(async x => await readFile(x))
-                 .map(x => rssParser(x));
+    var xml = url.map(async x => await readFile(x).then(j => j.map(k => rssParser(k)));
     var tmp = [].concat.apply([], xml);
 
     tmp.sort((a,b,) => Date.parse(b[2]) - Date.parse(a[2]));
