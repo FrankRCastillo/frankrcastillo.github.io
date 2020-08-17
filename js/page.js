dbf67ea440a5fcb4add5526d49d15115f3466c86
Frank Castillo <franklin.r.castillo.3@gmail.com>
2020-08-16 23:53:52 -0400

    update

--- js/page.js
+++ js/page.js
@@ -383,7 +383,8 @@ function newWindow(content) {
             let newx = oldx - d.clientX;
             let newy = oldy - d.clientY;
 
-            console.log(newx + '/' + newy);
+            this.style.top  = this.offsetTop  - ( newx + 'px');
+            this.style.left = this.offsetLeft - ( newy + 'px');
         };
 
         this.onmouseup = (f) => {
