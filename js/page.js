c765c45437dcbd0e4c78f26c1f7c5cdb209b1595
Frank Castillo <franklin.r.castillo.3@gmail.com>
2020-08-18 15:49:18 -0400

    update

--- js/page.js
+++ js/page.js
@@ -21,7 +21,7 @@ async function newConsole(consoleName) {
     ndiv.setattribute('class', 'newsdiv');
 
     nlnk.textContent = 'Toggle';
-    nlnk.addEventListener('click', async function () {
+    nlnk.addEventListener('click', () => {
         let news = await import('/apps/news.js');
         news.news();
     });
