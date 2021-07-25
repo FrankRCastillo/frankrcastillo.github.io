9991fe206a5222590040bf9d15a35923eda7fa6f
Frank Castillo <franklin.r.castillo.3@gmail.com>
2021-07-25 16:50:30 -0400

    fixed blog.js

--- apps/blog.js
+++ apps/blog.js
@@ -1,7 +1,7 @@
 // |apps|blog|Personal blog
 
 export function blog() {
-    let list = window.filelist.filter(/apps\/blog\/.*/);
+    let list = window.filelist.filter((x) => x.match('/apps\/blog\/.*/'));
 
     print(list);
 }
