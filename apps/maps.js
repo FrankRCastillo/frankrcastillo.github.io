c578c2c7d673af86eecfa949e20259afc48df89b
Frank Castillo <franklin.r.castillo.3@gmail.com>
2021-07-27 01:29:42 -0400

    updated maps

--- apps/maps.js
+++ apps/maps.js
@@ -8,21 +8,19 @@ export async function maps() {
 
 async function bmpToAscii() {
     let path = '/apps/maps/map.bmp';
-    let resp = await fetch(path);
-    let blob = await resp.blob()
-    let file = new File([blob], 'map.bmp', { type: 'image/bmp' });
     let img  = new Image();
     let cnvs = document.createElement('canvas');
     let cntx = cnvs.getContext('2d');
-    let img  = cntx.getImageData(0, 0, 1357, 628);
     let rtn  = '';
 
-    img.src  = await file.text();
+    img.src  = path;
 
     cntx.drawImage(img);
 
-    for (let i = 0; i < img.data.length; i++) {
-        rtn += img.data[i];
+    let data = cntx.getImageData(0, 0, 1357, 628);
+
+    for (let i = 0; i < data.length; i++) {
+        rtn += data[i];
 
         if (i + 1 % 1357 == 0) {
             rtn += '\n';
