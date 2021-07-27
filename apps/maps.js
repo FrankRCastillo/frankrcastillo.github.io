56d934436408f651156c4b1c780b1c72188c51e3
Frank Castillo <franklin.r.castillo.3@gmail.com>
2021-07-27 00:54:18 -0400

    created bmpToAscii

--- apps/maps.js
+++ apps/maps.js
@@ -1,8 +1,30 @@
 // |apps|maps|Map utility
 
 export async function maps() {
-    let path = '/apps/maps/map.png';
+    let txt = bmpToAscii();
+
+    print(txt);
+}
+
+async function bmpToascii() {
+    let path = '/apps/maps/map.bmp';
     let resp = await fetch(path);
     let blob = await resp.blob()
     let file = new File([blob], 'map.png', { type: 'image/png' });
+    let cnvs = document.createElement('canvas');
+    let cntx = cnvs.getContext('2d');
+    let img  = cntx.getImageData(0, 0, 1357, 628);
+    let rtn  = '';
+
+    img.src  = await file.text();
+
+    for (let i = 0; i < img.data.length; i++) {
+        rtn += img.data[i];
+
+        if (i + 1 % 1357 == 0) {
+            rtn += '\n';
+        }
+    }
+
+    return rtn;
 }
