1029706b14d9a8b02ceb1cae05a5784b99fd3941
Frank Castillo <franklin.r.castillo.3@gmail.com>
2021-07-27 21:42:09 -0400

    update maps

--- apps/maps.js
+++ apps/maps.js
@@ -8,20 +8,17 @@ export async function maps() {
 
 async function bmpToAscii() {
     let rtn = '';
-    let bmp = '/apps/maps/map.bmp';
-    let img = new Image();
-    let cnv = document.createElement('canvas');
-    let ctx = cnv.getContext('2d');
-    let fch = await fetch(bmp);
-    let blb = await fch.blob();
+    let url = '/apps/maps/map.bmp';
+    let rsp = await fetch(url);
+    let fbl = await rsp.blob();
+    let bmp = await createImageBitmap(fbl);
+    let cnv = new OffscreenCanvas(bmp.width, bmp.height);
+    let ctx = canvas.getContext('2d');
 
-    img.src = blb;
-    cnv.width = img.width;
-    cnv.height = img.height;
-    ctx.drawImage(img, 0, 0, img.height, img.width);
+    ctx.drawImage(bmp, 0, 0);
 
-    let dat = ctx.getImageData(0, 0, img.height, img.width).data;
-
+    var dat = ctx.getImageData(0, 0, bmp.width, bmp.height);
 
     return rtn;
+
 }
