b474b432b127f0a9ee0f51cb149e6fcf9a7b17cf
Frank Castillo <franklin.r.castillo.3@gmail.com>
2021-10-11 12:19:38 -0400

    updted getCmdInfo

--- js/page.js
+++ js/page.js
@@ -227,8 +227,6 @@ function arrayToTable(arr) {
 
 async function getCmdInfo() {
     let list = window.filelist.filter((x) => x.match('apps/.*.js'));
-    let dir  = '';
-    let app  = '';
     let lout = await Promise.all(list.map(async x => {
         let info = x.replace('.js').split('/');
         getJsDesc( info[0]
