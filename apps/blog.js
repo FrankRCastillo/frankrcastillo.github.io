d2560954bf36f00cf1da9859d192f28581e4aff6
Frank Castillo <franklin.r.castillo.3@gmail.com>
2021-10-11 17:34:28 -0400

    changed blog

--- apps/blog.js
+++ apps/blog.js
@@ -3,10 +3,9 @@
 export function blog() {
     let list = window.filelist.filter((x) => x.match('apps/blog/'));
     let urls = list.map((x) => {
-        let url = 'https://frankrcastillo.github.io/' + x;
         let lnk = document.createElement('a');
 
-        lnk.setAttribute('href', url);
+        lnk.setAttribute('onClick', 'print(await read(' + x + '))');
         lnk.text = x;
 
         return lnk;
