46676230915e29a065fbc888740556ed1b6d4817
Frank Castillo <franklin.r.castillo.3@gmail.com>
2021-10-13 17:25:01 -0400

    changed prompt and css

--- apps/blog.js
+++ apps/blog.js
@@ -1,22 +1,17 @@
 // Personal blog
 
-export function blog() {
+async export function blog() {
     let path = 'apps/blog/';
     let list = window.filelist.filter((x) => x.match(path));
     let urls = list.map((x) => {
-        let lnk = document.createElement('a');
-
-        lnk.text = x.replace(path, '');
+        let lnk  = document.createElement('a');
+        let file = x.replace(path, '');
+        lnk.text = file;
         lnk.setAttribute('href', 'javascript:void(0)');
         lnk.addEventListener('click', (e) => {
-            e = e || window.event;
-
-            console.log(e.target.text);
-            // let url = e.text;
-            // let txt = await read(url);
-            // print(txt);
+            let txt = await read(x);
+            print(txt);
         });
-
 
         return lnk;
     });
