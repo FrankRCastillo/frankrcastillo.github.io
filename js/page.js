a10dd55d5bf7747442c45600c14dba3a1181db1b
Frank Castillo <franklin.r.castillo.3@gmail.com>
2021-10-13 23:12:39 -0400

    changed prompt and css

--- js/page.js
+++ js/page.js
@@ -31,7 +31,8 @@ async function cmdManager(input) {
         print(promptmsg + input, true);
 
         try {
-            eval('window.apps[\"apps/' + cmd + '.js\"]' + cmd + '()');
+            let app = 'apps/' + cmd + '.js';
+            eval('window.apps[' + app +']' + cmd + '()');
         } catch(e) {
             print(cmd + ': command not found...');
         }
