3fcd2ca5bdf22aeb6a06dd68120e7b3c2105e32f
franklin.r.castillo.3@gmail.com <franklin.r.castillo.3@gmail.com>
2025-05-12 11:11:26 -0400

    Updates

--- js/projects.js
+++ js/projects.js
@@ -1,42 +1,49 @@
+function parseCSV(text) {
+    return text
+        .trim()
+        .split('\n')
+        .map(line => {
+            return line.match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g)?.map(field =>
+                field.replace(/^"(.*)"$/, '$1').trim()
+            ) || [];
+        });
+}
+
 async function load_projects() {
-    const blogList = document.getElementById('projectsList');
-    const api      = `https://api.github.com/repos/FrankRCastillo/frankrcastillo.github.io/contents/blog?ref=master`;
+    const projList = document.getElementById('projectsList');
+    const api      = `https://api.github.com/repos/FrankRCastillo/frankrcastillo.github.io/contents/pages/projects.csv?ref=master`;
     const res      = await fetch(api);
 
     if (!res.ok) {
-        blogList.innerHTML = '<p>Error loading posts.</p>';
+        blogList.innerHTML = '<p>Error loading projects.</p>';
         return;
     }
 
-    const files = await res.json();
-    blogList.innerHTML = '';
+    const files = await res.text();
+    const rows  = praseCSV(files);
 
-    files.filter(f => f.name.endsWith('.txt')).forEach(file => {
-        const link = document.createElement('a');
+    ul = document.createElement('ul')
 
-        link.href = '#';
+    rows.forEach(row => {
+        name = row[0];
+        link = row[1];
+        desc = row[2];
 
-        link.textContent = file.name.replace('.txt', '');
+        li     = document.createElement('li');
+        strong = document.createElement('strong');
+        atag   = document.createElement('a');
 
-        link.onclick = async (e) => {
-            e.preventDefault();
-            const res = await fetch(file.download_url);
+        atag.textContent = name;
+        atag.href = link;
 
-            if (!res.ok) {
-                blogList.innerHTML = '<p>Error loading post.</p>';
-                return;
-            }
 
-            const text = await res.text();
 
-            blogList.innerHTML = `<pre>${text}</pre>`;
-        };
-
-        const div = document.createElement('div');
-
-        div.appendChild(link);
-
-        blogList.appendChild(div);
+        strong.appendChild(atag);
+        li.appendChild(strong);
+        li.appendChild(desc);
+        ul.appendchild(li);
     });
+
+    projList.appendChild(ul);
 }
 
