4901186383c06f791f0929cc57ecb1367adcab6e
franklin.r.castillo.3@gmail.com <franklin.r.castillo.3@gmail.com>
2025-05-12 23:57:58 -0400

    updated cmd in main

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ async function loadPage(url, pageName) {
 
     // If this is the terminal page, load and run the module
     if (pageName === 'cmd') {
-        const module = await import(`./js/cmd.js`);
+        const module = await import(`./cmd.js`);
 
         console.log("cmd.js loaded");
 
