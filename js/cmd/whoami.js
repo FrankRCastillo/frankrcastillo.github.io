183f9d1192cb478bf136b8715c07b048681a1dba
franklin.r.castillo.3@gmail.com <franklin.r.castillo.3@gmail.com>
2025-05-14 12:59:53 -0400

    modified whoami to cache ip data

--- js/cmd/whoami.js
+++ js/cmd/whoami.js
@@ -4,23 +4,25 @@ export default async function whoami() {
     const key = 'whoami-cache';
 
     const cached = sessionStorage.getItem(key);
-    if (cached) return cached;
+
+    if (cached) {
+       return cached;
+    }
 
     try {
         const res  = await fetch('https://ipinfo.io/json');
         const data = await res.json();
 
-        const output = [
-            `IP: ${data.ip}`,
-            `Hostname: ${data.hostname}`,
-            `City: ${data.city}`,
-            `Postal: ${data.postal}`,
-            `Region: ${data.region}`,
-            `Country: ${data.country}`,
-            `Location: ${data.loc}`,
-            `Org: ${data.org}`,
-            `Timezone: ${data.timezone}`
-        ].join('\n');
+        const output = [ `IP       : ${data.ip}`
+                       , `Hostname : ${data.hostname}`
+                       , `City     : ${data.city}`
+                       , `Postal   : ${data.postal}`
+                       , `Region   : ${data.region}`
+                       , `Country  : ${data.country}`
+                       , `Location : ${data.loc}`
+                       , `Org      : ${data.org}`
+                       , `Timezone : ${data.timezone}`
+                       ].join('\n');
 
         sessionStorage.setItem(key, output);
 
