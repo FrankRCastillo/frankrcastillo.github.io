47f08f4d642ce767fb41dc0d84c973836d0bdb93
franklin.r.castillo.3@gmail.com <franklin.r.castillo.3@gmail.com>
2025-05-14 12:57:57 -0400

    modified whoami to cache ip data

--- js/cmd/whoami.js
+++ js/cmd/whoami.js
@@ -1,23 +1,32 @@
 export const description = "Shows your IP and geolocation info.";
 
 export default async function whoami() {
+    const key = 'whoami-cache';
+
+    const cached = sessionStorage.getItem(key);
+    if (cached) return cached;
+
     try {
         const res  = await fetch('https://ipinfo.io/json');
         const data = await res.json();
 
-        return [ `IP: ${data.ip}`
-               , `Hostname: ${data.hostname}`
-               , `City: ${data.city}`
-               , `Postal: ${data.postal}`
-               , `Region: ${data.region}`
-               , `Country: ${data.country}`
-               , `Location: ${data.loc}`
-               , `Org: ${data.org}`
-               , `Timezone: ${data.timezone}`
-               ].join('\n');
+        const output = [
+            `IP: ${data.ip}`,
+            `Hostname: ${data.hostname}`,
+            `City: ${data.city}`,
+            `Postal: ${data.postal}`,
+            `Region: ${data.region}`,
+            `Country: ${data.country}`,
+            `Location: ${data.loc}`,
+            `Org: ${data.org}`,
+            `Timezone: ${data.timezone}`
+        ].join('\n');
+
+        sessionStorage.setItem(key, output);
+
+        return output;
 
     } catch (err) {
         return `whoami: failed to retrieve IP info`;
     }
 }
-
