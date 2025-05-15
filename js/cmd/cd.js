c965e9e3a13220c3e6bc57e7fefce7875b23cdef
franklin.r.castillo.3@gmail.com <franklin.r.castillo.3@gmail.com>
2025-05-15 00:51:46 -0400

    code cleanup

--- js/cmd/cd.js
+++ js/cmd/cd.js
@@ -2,9 +2,10 @@ export const description = "Changes the working directory.";
 
 export default async function cd(args, base, stdin = '') {
     const path = args[0];
-    if (!path) return 'cd: missing directory';
 
-if (!window.pathStack) window.pathStack = [];
+    if (!path) { return 'cd: missing directory'; }
+
+    if (!window.pathStack) { window.pathStack = []; }
 
     let newStack = [...window.pathStack];
 
