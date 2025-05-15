89f0d4c5f3b4f8f153f56570df969f481a1aee2b
franklin.r.castillo.3@gmail.com <franklin.r.castillo.3@gmail.com>
2025-05-15 00:52:38 -0400

    cleared pathStack in umount

--- js/cmd/umount.js
+++ js/cmd/umount.js
@@ -1,8 +1,9 @@
 export const description = "Unmount current repo and return to default.";
 
 export default function umount() {
-    window.repoName = window.defaultRepoName;
-    window.repoBase = window.defaultRepoBase;
+    window.repoName  = window.defaultRepoName;
+    window.repoBase  = window.defaultRepoBase;
+    window.pathStack = [];
     
     return `Repo reset to ${window.repoName}`;
 }
