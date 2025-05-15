ad8ee801c506807a5ac90be7291379f25824684c
franklin.r.castillo.3@gmail.com <franklin.r.castillo.3@gmail.com>
2025-05-15 05:12:48 -0400

    updated umount

--- js/cmd/umount.js
+++ js/cmd/umount.js
@@ -1,6 +1,11 @@
 export const description = "Unmount current repo and return to default.";
 
 export default function umount() {
+    if ( window.repoName === window.defaultRepoName &&
+         window.repoBase === window.defaultRepoBase ) {
+        return 'already using default repo (cwd preserved)';
+    }
+    
     window.repoName  = window.defaultRepoName;
     window.repoBase  = window.defaultRepoBase;
     window.pathStack = [];
